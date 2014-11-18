02ddea59ae0c37eb79e7ea82512050c7dbe077ce
feng32tc@gmail.com <feng32tc@gmail.com@a88f8048-e03e-4f35-9302-863601e098a4>
2014-11-18 14:44:15 +0000

    Add Engine api

---
+++ trunk/src/Engine/Engine.h
@@ -0,0 +1,55 @@
+#ifndef ENGINE_H
+#define ENGINE_H
+
+struct RtPoint
+{
+    double x;
+    double y;
+    double z;
+};
+
+struct RtVector
+{
+    double x;
+    double y;
+    double z;
+};
+
+struct RtTriangle
+{
+    RtPoint a;
+    RtPoint b;
+    RtPoint c;
+    RtVector n;
+};
+
+enum RtPreprocessMethod
+{
+    Linear,
+    Grid,
+    KdTree
+};
+
+void Initialize();
+
+void AddTriangle(const RtTriangle &triangle);
+void AddTriangles(const RtTriangle *triangles, int n);
+bool AddStlModel(const char *filename); // TODO: add unicode version
+
+bool Preprocess(RtPreprocessMethod m);
+
+void SetTxPoint(const RtPoint &point, double power); // power in dBm
+void SetRxPoints(const RtPoint *points, int n, double radius); // radius in meters
+
+void SetParameters(
+    double permittivity,
+    double conductivity,
+    int maxReflections,
+    double raySpacing,   // unit: degree
+    double frequency     // unit: MHz
+    );
+
+bool Simulate();
+bool GetRxPowers(double *powers, int n);
+
+#endif
